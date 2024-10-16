92d447edf87ed31b070709c52b409bef8f17130a
Utkarsh Agrawal <utarsh.agrawal@publicissapient.com>
2024-10-16 13:28:36 +0530

    Updated to fix build error(s) & include scan QR functionality

--- main_app/features/presentation/lib/credential_exchange/widgets/wallet_credentials_offer_request_widget.dart
+++ main_app/features/presentation/lib/credential_exchange/widgets/wallet_credentials_offer_request_widget.dart
@@ -1,12 +1,17 @@
+import 'package:core/qr_code/qr_code_scanner.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_dependencies/constants/color_constants.dart';
 import 'package:shared_dependencies/constants/dimension_constants.dart';
-import 'package:widget_library/input_text_view/form_text_field.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class CredentialOfferInputFormAttributes {
+  String offerRequestData;
   ValueSetter<String> issueOfferRequestInputForm;
   Function submitButtonPressed;
-  CredentialOfferInputFormAttributes({required this.issueOfferRequestInputForm, required this.submitButtonPressed});
+  CredentialOfferInputFormAttributes(
+      {required this.offerRequestData,
+        required this.issueOfferRequestInputForm,
+        required this.submitButtonPressed});
 }
 
 class CredentialOfferInputFormWidget extends StatelessWidget {
@@ -19,76 +24,50 @@ class CredentialOfferInputFormWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController textEditingController = TextEditingController();
-    ScrollController scrollController = ScrollController();
-    const radius = Radius.circular(DimensionConstants.size22);
-    return Container(
-      height: (MediaQuery.of(context).size.height),
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.vertical(
-          top: radius,
-        ),
-        color: ColorConstants.COLOR_WHITE,
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: DimensionConstants.size20,
+        right: DimensionConstants.size20,
       ),
       child: Column(
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: DimensionConstants.size20,
-                      right: DimensionConstants.size20,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: DimensionConstants.size180,
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: ColorConstants.COLOR_FF188006,
-                              width: DimensionConstants.size1,
-                            ),
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(DimensionConstants
-                                  .size4), //                 <--- border radius here
-                            ),
-                          ),
-                          child: Scrollbar(
-                            controller: scrollController,
-                            child: FormTextField(
-                              maxLines: 6,
-                              backgroundColor: ColorConstants.COLOR_52E0E0E0,
-                              isRtl: true,
-                              controller: textEditingController,
-                              onSubmitted:
-                              attributes.issueOfferRequestInputForm,
-                              onChanged: attributes.issueOfferRequestInputForm,
-                              hint: "Please enter openid-credential-offer",
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: DimensionConstants.size100,
-                        ),
-                        ElevatedButton(
-                          onPressed: (){
-                            attributes.submitButtonPressed.call();
-                          },
-                          child: const Text('Submit'),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+          Container(
+            height: DimensionConstants.size350,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: ColorConstants.COLOR_FF188006,
+                width: DimensionConstants.size1,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(DimensionConstants
+                    .size4), //                 <--- border radius here
               ),
             ),
+            child: attributes.offerRequestData == ''
+                ? QRCodeScanner(
+              attributes: QRCodeScannerAttributes(onQrDetect: (capture) {
+                final List<Barcode> barcodes = capture.barcodes;
+                for (final barcode in barcodes) {
+                  if (barcode.rawValue != '') {
+                    attributes
+                        .issueOfferRequestInputForm(barcode.rawValue!);
+                  }
+                }
+              }),
+            )
+                : Text(attributes.offerRequestData),
           ),
+          const SizedBox(
+            height: DimensionConstants.size100,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              attributes.offerRequestData == ''
+                  ? null
+                  : attributes.submitButtonPressed.call();
+            },
+            child: const Text('Submit'),
+          )
         ],
       ),
     );
